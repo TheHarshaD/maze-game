1cdb73f589a422bd5c1088672ee439daa8f5f7c1
Harsha Dantuluri <hdantuluri@gmail.com>
2023-08-08 03:39:18 -0700

    remove works now for bin min heap

--- binary-heap.js
+++ binary-heap.js
@@ -12,12 +12,19 @@ class MinHeap {
     
     //always removes min element 
     remove(){
-        if (this.isEmpty()) {
+        if (this.checkIfEmpty()) {
             return null;
           }
           
           const min = this.heap[0];
           const last = this.heap.pop();
+
+          if (!this.checkIfEmpty()) {
+            this.heap[0] = last;
+            this.bubbleDown(0);
+          }
+          
+          return min;
     }
     //returns length of heap/array
     size(){
@@ -32,13 +39,28 @@ class MinHeap {
     //move element up after its been placed at end off tree/array (if needs)
     //will always be last element in array
     bubbleUp(index) {
-        //console.log(index);
         let parentIndex = Math.floor((index-1)/2);
         while(index > 0 && this.heap[index] < this.heap[parentIndex]){
-            console.log('came here');
             [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
             index = parentIndex;
             parentIndex = Math.floor((index - 1) / 2);
+        }
+    }
+
+    bubbleDown(index) {
+        const leftChildIndex = 2 * index + 1;
+        const rightChildIndex = 2 * index + 2;
+        let minIndex = index;
+
+        if (leftChildIndex < this.heap.length && this.heap[leftChildIndex] < this.heap[minIndex]) {
+            minIndex = leftChildIndex;
+        }
+        if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] < this.heap[minIndex]) {
+            minIndex = rightChildIndex;
+        }
+        if (minIndex !== index) {
+            [this.heap[index], this.heap[minIndex]] = [this.heap[minIndex], this.heap[index]];
+            this.bubbleDown(minIndex);
         }
     }
 
@@ -48,7 +70,6 @@ class MinHeap {
     
       printTreeNode(index, prefix, childPrefix) {
         if (index < this.heap.length) {
-          console.log(prefix + this.heap[index]);
           const leftChildIndex = 2 * index + 1;
           const rightChildIndex = 2 * index + 2;
           if (leftChildIndex < this.heap.length || rightChildIndex < this.heap.length) {
@@ -68,14 +89,14 @@ minHeap.insert(10);
 minHeap.insert(5);
 minHeap.insert(20);
 minHeap.insert(3);
-//minHeap.insert(8);
+minHeap.insert(8);
 
 console.log('Heap after insertions:', minHeap.heap);
 
 // Extract the minimum element from the heap
-//const minElement = minHeap.extractMin();
-//console.log('Extracted min element:', minElement);
-//console.log('Heap after extraction:', minHeap.heap);
+const minElement = minHeap.remove();
+console.log('Extracted min element:', minElement);
+console.log('Heap after extraction:', minHeap.heap);
 
 // Check the size and emptiness of the heap
 console.log('Heap size:', minHeap.size());
