4206e4c1e0e5ba632dbac990c0d09cf720a5fd61
Harsha Dantuluri <hdantuluri@gmail.com>
2023-08-07 21:00:16 -0700

    initial grid setup - 2d array

--- maze.js
+++ maze.js
@@ -1,6 +1,7 @@
 let maze = document.querySelector(".maze");
 let ctx = maze.getContext("2d");
 
+let current;
 
 
 class Maze{
@@ -11,10 +12,35 @@ class Maze{
         this.grid = [];
         this.stack = [];
     }
+
+    setup(){
+        for(let r=0; r < this.rows; r++){
+            let row = [];
+            for(let c=0; c < this.cols; c++){
+                let cell = new Cell(r, c, this.grid, this.size);
+                row.push(cell);
+            }
+            this.grid.push(row);
+        }
+        current = this.grid[0][0];
+    }
 }
 
 class Cell{
-    constructor(rowN, colN, parentGrid){
-        
+    constructor(rowNum, colNum, parentGrid, parentSize){
+        this.rowNum = rowNum; 
+        this.colNum = colNum; 
+        this.parentGrid = parentGrid; 
+        this.parentSize = parentSize;
+        this.visited = false;
+        this.walls = {
+            topWall: true,
+            rightWall: true,
+            bottomWall: true,
+            leftWall: true
+        };
     }
 }
+
+let newMaze = new Maze(500, 10, 10);
+newMaze.setup();
